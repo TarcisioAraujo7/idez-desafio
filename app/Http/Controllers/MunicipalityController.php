600d660aaa3ed804995ef6c09d9ec8a563d60330
Tarcisio Almeida <tarcisio.olv@gmail.com>
2025-10-19 22:00:30 -0300

    feat: habilita lógica de paginação em controller

--- app/Http/Controllers/MunicipalityController.php
+++ app/Http/Controllers/MunicipalityController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Services\MunicipalityService;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Services\MunicipalityService;
 
 class MunicipalityController extends Controller
 {
     public function __construct(private MunicipalityService $service) {}
 
-    public function index(string $uf): ?JsonResponse
+    public function index(Request $request, string $uf): ?JsonResponse
     {
+        $page = $request->query('page');
+        $perPage = $request->query('per_page');
+
+        $result = $this->service->index($uf, $page, $perPage);
         return response()->json(
-            data: $this->service->index($uf)
+            data: $result
         );
     }
 }
